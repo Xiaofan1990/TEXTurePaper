change texture default color, which somehow fixed gradient loss during projecting

scripts/debug_color.py
@@ -12,7 +12,7 @@ from src import utils
 def load(path):
     image = load_image(path)
     tensor = utils.image2tensor_affecting_input(np.array(image))
-    tensor = tensor[:, :, 745:755, 970:980]
+    #tensor = tensor[:, :, 745:755, 970:980]
     print(path)
     print(str(tensor[-1, 1:3, :, :])+"\n")
     return tensor
@@ -24,7 +24,7 @@ load(
 load(
     "C:/Users/xiaof/TEXTurePaper/experiments/test/0006_0254_project_update.jpg")
 tensor = load(
-    "C:/Users/xiaof/TEXTurePaper/experiments/test/0006_0279_fitted.jpg")
+    "C:/Users/xiaof/TEXTurePaper/experiments/test/step_00010_texture.png")
 
 # tensor = F.interpolate(tensor, (120, 120), mode='nearest')
 image = Image.fromarray(utils.tensor2img_affecting_input(tensor))

src/models/textured_mesh.py
@@ -125,7 +125,11 @@ class TexturedMeshModel(nn.Module):
         self.renderer = Renderer(device=self.device, dim=(render_grid_size, render_grid_size),
                                                            interpolation_mode=self.opt.texture_interpolation_mode)
         self.env_sphere, self.mesh = self.init_meshes()
-        self.default_color = [0.8, 0.1, 0.8]
+        # TODO this default color can cause G color to loss gradient when project back. Not sure why. Maybe color became negative?
+        # self.default_color = [0.8, 0.1, 0.8]
+   
+        self.default_color = [0.5, 0.5, 0.5]
+        
         self.background_sphere_colors, self.texture_img = self.init_paint()
         self.meta_texture_img = nn.Parameter(torch.zeros_like(self.texture_img))
         if self.opt.reference_texture:
@@ -266,7 +270,12 @@ class TexturedMeshModel(nn.Module):
                 list(color)).float().to(A.device)
         return init_color_in_latent
 
+    def get_unpaint_mask(self, size_map_cache):
+        return (size_map_cache[:,:1,:,:] < 1e-3).float()
+
     def change_default_to_median(self):
+        return
+        # TODO remove this.
         diff = (self.texture_img - torch.tensor(self.default_color).view(1, 3, 1, 1).to(
             self.device)).abs().sum(axis=1)
         default_mask = (diff < 0.1).float().unsqueeze(0)
@@ -399,15 +408,17 @@ class TexturedMeshModel(nn.Module):
 
         augmented_vertices = self.mesh.vertices
 
-        if use_median:
-            diff = (texture_img - torch.tensor(self.default_color).view(1, 3, 1, 1).to(
-                self.device)).abs().sum(axis=1)
-            default_mask = (diff < 0.1).float().unsqueeze(0)
-            median_color = texture_img[0, :].reshape(3, -1)[:, default_mask.flatten() == 0].mean(
-                axis=1)
-            texture_img = texture_img.clone()
-            with torch.no_grad():
-                texture_img.reshape(3, -1)[:, default_mask.flatten() == 1] = median_color.reshape(-1, 1)
+        # TODO Xiaofan why do we need this? to prevent color leakage. E.g impaint will follow the color tone of default color if we don't play with masks properly
+        #if use_median:
+        #    # This is ruining everything since we changed default color to grey, which is commonly used.
+        #    diff = (texture_img - torch.tensor(self.default_color).view(1, 3, 1, 1).to(
+        #        self.device)).abs().sum(axis=1)
+        #    default_mask = (diff < 0.1).float().unsqueeze(0)
+        #    median_color = texture_img[0, :].reshape(3, -1)[:, default_mask.flatten() == 0].mean(
+        #        axis=1)
+        #    texture_img = texture_img.clone()
+        #    with torch.no_grad():
+        #        texture_img.reshape(3, -1)[:, default_mask.flatten() == 1] = median_color.reshape(-1, 1)
         background_type = 'none'
         use_render_back = False
         if background is not None and type(background) == str:

src/training/trainer.py
@@ -427,12 +427,7 @@ class TEXTure:
                                          dims=(dim, dim), background='white')
         z_normals = outputs['normals'][:, -1:, :, :].clamp(0, 1)
         rgb_render = outputs['image']  # .permute(0, 2, 3, 1).contiguous().clamp(0, 1)
-        diff = (rgb_render.detach() - torch.tensor(self.mesh_model.default_color).view(1, 3, 1, 1).to(
-            self.device)).abs().sum(axis=1)
-        uncolored_mask = (diff < 0.1).float().unsqueeze(0)
-        rgb_render = rgb_render * (1 - uncolored_mask) + utils.color_with_shade([0.85, 0.85, 0.85], z_normals=z_normals,
-                                                                                light_coef=0.3) * uncolored_mask
-
+        
         outputs_with_median = self.mesh_model.render(theta=theta, phi=phi, radius=radius,
                                                      dims=(dim, dim), use_median=True,
                                                      render_cache=outputs['render_cache'])
@@ -440,6 +435,10 @@ class TEXTure:
         meta_output = self.mesh_model.render(theta=theta, phi=phi, radius=radius,
                                              background=torch.Tensor([0, 0, 0]).to(self.device),
                                              use_meta_texture=True, render_cache=outputs['render_cache'])
+        size_map_cache = meta_output['image'].clamp(0, 1)
+        uncolored_mask = (size_map_cache[:,:1,:,:] < 1e-3).float()
+        rgb_render = rgb_render * (1 - uncolored_mask) + utils.color_with_shade([0.85, 0.85, 0.85], z_normals=z_normals,
+                                                                                light_coef=0.3) * uncolored_mask
         pred_z_normals = meta_output['image'][:, :1].detach()
         rgb_render = rgb_render.permute(0, 2, 3, 1).contiguous().clamp(0, 1).detach()
         texture_rgb = outputs_with_median['texture_map'].permute(0, 2, 3, 1).contiguous().clamp(0, 1).detach()
@@ -472,17 +471,12 @@ class TEXTure:
                          mask: torch.Tensor):
         object_mask = torch.ones_like(depth_render)
         object_mask[depth_render == 0] = 0
+        exact_generate_mask = (size_map_cache[:,:1,:,:] < 1e-3).float()
+        exact_generate_mask[object_mask==0] = 0
         # erode object mask to avoid background color leakaging into our texture. This mostly only happen when init latent is not randomized.
         object_mask = torch.from_numpy(
             cv2.erode(object_mask[0, 0].detach().cpu().numpy(), np.ones((7, 7), np.uint8))).to(
             object_mask.device).unsqueeze(0).unsqueeze(0)
-        #TODO this is not robust at all
-        diff = (rgb_render_raw.detach() - torch.tensor(self.mesh_model.default_color).view(1, 3, 1, 1).to(
-            self.device)).abs().sum(axis=1)
-        exact_generate_mask = (diff < 0.1).float().unsqueeze(0)
-
-
-
         
         # small area kernel size shold at least be render_resolution/vae_input_size, which is 512 ??
         small_are_kernel_size = math.ceil(self.cfg.render.train_grid_size/512)
@@ -687,11 +681,11 @@ class TEXTure:
         return fitted_rgb, fitted_size_map
     
     def log_part_image(self, tensor: torch.Tensor,  name: str, colormap=False):
-        part_tensor = tensor[:, :, 1400:1410, 1420:1430]
-        scaled_part = torch.nn.functional.interpolate(input = part_tensor, scale_factor = (16, 16))
-        self.log_train_image(scaled_part, name, colormap)
+        #part_tensor = tensor[:, :, 1400:1410, 1420:1430]
+        #scaled_part = torch.nn.functional.interpolate(input = part_tensor, scale_factor = (16, 16))
+        #self.log_train_image(scaled_part, name, colormap)
 
-        #self.log_train_image(tensor, name, colormap)
+        self.log_train_image(tensor, name, colormap)
 
     def log_part_loss(self, a: torch.Tensor , b: torch.Tensor, name):
         x = a[:, :, 1400:1410, 1420:1430]